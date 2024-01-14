972d396d99438d926c3954839b31fc120e3448f5
OtsoBear <otso.veistera@gmail.com>
2024-01-14 12:41:18 +0200

    Fix audio

--- characters/player/head.gd
+++ characters/player/head.gd
@@ -24,20 +24,20 @@ func _process(delta):
 	
 	progress_bar.value = oxygen_amount
 	if oxygen_amount < 10 && !%AudioStreamPlayer.is_playing():
-		%AudioStreamPlayer.stream = "res://sounds/hukkuminen.mp3"
+		%AudioStreamPlayer.stream = load("res://sounds/hukkuminen.mp3")
 		%AudioStreamPlayer.play() 
 		if !%AudioStreamPlayer.is_playing():
-			%AudioStreamPlayer.stream = "res://sounds/pool noodle.mp3"
+			%AudioStreamPlayer.stream = load("res://sounds/pool noodle.mp3")
 			%AudioStreamPlayer.play()
 	if oxygen_amount == 76 && !%AudioStreamPlayer.is_playing():
-		%AudioStreamPlayer.stream ="res://sounds/murinaa (matala).mp3" 
+		%AudioStreamPlayer.stream =load("res://sounds/murinaa (matala).mp3" )
 		%AudioStreamPlayer.play()
 	if oxygen_amount == 52 && !%AudioStreamPlayer.is_playing():
-		%AudioStreamPlayer.stream ="res://sounds/katriina rummuttaa vihaisesti altaan reunalla.mp3"
+		%AudioStreamPlayer.stream =load("res://sounds/katriina rummuttaa vihaisesti altaan reunalla.mp3")
 		%AudioStreamPlayer.play()
 	
 	if oxygen_amount == 28 && !%AudioStreamPlayer.is_playing():
-		%AudioStreamPlayer.stream =  "res://sounds/kupla 2.mp3"
+		%AudioStreamPlayer.stream =  load("res://sounds/kupla 2.mp3")
 		%AudioStreamPlayer.play()
 
 func _physics_process(delta):
@@ -58,14 +58,14 @@ func isInWater(transform: Vector3) -> bool:
 func enter_oxygen_pocket():
 	inside_oxygen_pocket = true
 	if !%AudioStreamPlayer.is_playing():
-		%AudioStreamPlayer.stream = "res://sounds/kuplii.mp3"
+		%AudioStreamPlayer.stream = load("res://sounds/kuplii.mp3")
 		%AudioStreamPlayer.play()
 	
 func exit_oxygen_pocket():
 	inside_oxygen_pocket = false
 
 	if !%AudioStreamPlayer.is_playing():
-		%AudioStreamPlayer.stream = "res://sounds/vesi splash splash.mp3"
+		%AudioStreamPlayer.stream = load("res://sounds/vesi splash splash.mp3")
 		%AudioStreamPlayer.play()
 
 
